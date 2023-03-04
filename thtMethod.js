1e18b39fea9036a1da867c5a69e7e18b98fc41d9
yudiPradnyaPolvi <pradnyakemenuh@gmail.com>
2023-03-04 17:37:16 +0800

    first commit

---
+++ thtMethod.js
@@ -0,0 +1,279 @@
+console.log("20 PROCEDURE untuk menyimpan nilai untuk 20 variable")
+console.log ("Data Mahasiswa Elektro 2020")
+let nim  = "2005541146"
+let nama ="yudi"
+let tinggiBadan="165"
+let beratBadan=" 75 "
+let usia=" 20 "
+let golonganDarah=" O "
+let nomorTelepon=" 082123456789 "
+let agama=" Hindu "
+let tanggalLahir=" 12 September 2001 "
+let asal =" Bali "
+let suku =" Bali "
+let asalSekolah=" Sman 1 Kuta "
+let prestasi="Juara 1 Lomba Tarik Tambang"
+let jalurMasuk="SNMPTN"
+const tanggalMasuk="1 Agustus 2020"
+let ipk="3.95"
+const jurusan="Teknik Elektro"
+const fakultas="Teknik"
+const universitas="Udayana"
+let situasi="Aktif"
+
+//declare function here
+function dataMahasiswa(nim,nama,tinggiBadan,beratBadan,usia,golonganDarah,nomorTelepon,agama,tanggalLahir,asal,suku,asalSekolah,prestasi,jalurMasuk,tanggalMasuk,ipk,jurusan,fakultas,universitas,situasi){
+console.log(nim+"-"+nama +"-"+tinggiBadan +"-"+beratBadan +"-"+usia +"-"+golonganDarah +"-"+nomorTelepon +"-"+agama+"-"+tanggalLahir +"-"+asal +"-"+suku +"-"+asalSekolah +"-"+prestasi +"-"+jalurMasuk +"-"+tanggalMasuk +"-"+ipk +"-"+jurusan +"-"+fakultas +"-"+universitas +"-"+situasi)
+}
+
+//call function here
+dataMahasiswa(nim+"-"+nama +"-"+tinggiBadan +"-"+beratBadan +"-"+usia +"-"+golonganDarah +"-"+nomorTelepon +"-"+agama +"-"+tanggalLahir +"-"+asal +"-"+suku +"-"+asalSekolah +"-"+prestasi +"-"+jalurMasuk +"-"+tanggalMasuk +"-"+ipk +"-"+jurusan +"-"+fakultas +"-"+universitas +"-"+situasi)
+
+
+
+console.log ("Data Dosen Teknik Elektro")
+//inisiasi Variable
+let nip  = "122005541107"
+let namaDosen ="Ari"
+let tinggiBadanDosen ="170"
+let beratBadanDosen="90"
+let usiaDosen="50"
+let golonganDarahDosen=" O "
+let nomorTeleponDosen=" 082123456789 "
+let agamaDosen=" Islam "
+let tanggalLahirDosen=" 12 September 2001 "
+let asalDosen =" Lombok "
+let sukuDosen =" Lombok "
+let asalLulusanDosen=" Universitas Mataram "
+let prestasiDosen="Juara 1 Lomba Melukis"
+let gelar ="Sarjana Teknik Elektro"
+let tanggalMulaiMengajar="1 Agustus 2020"
+let golongan ="IIA"
+const jurusanMengajar="Teknik Elektro"
+const fakultasMengajar="Teknik"
+const universitasMengajar="Udayana"
+let situasiDosen="Aktif"
+
+//declare function here
+function dataDosen(nip,namaDosen,tinggiBadanDosen,beratBadanDosen,usiaDosen,golonganDarahDosen,nomorTeleponDosen,agamaDosen,tanggalLahirDosen,asalDosen,sukuDosen,asalLulusanDosen,prestasiDosen,gelar,tanggalMulaiMengajar,golongan,jurusanMengajar,fakultasMengajar,universitasMengajar,situasiDosen){
+console.log(nip+"-"+namaDosen +"-"+tinggiBadanDosen +"-"+beratBadanDosen +"-"+usiaDosen +"-"+golonganDarahDosen +"-"+nomorTeleponDosen +"-"+agamaDosen+"-"+tanggalLahirDosen +"-"+asalDosen +"-"+sukuDosen +"-"+asalLulusanDosen +"-"+prestasiDosen +"-"+gelar +"-"+tanggalMulaiMengajar +"-"+golongan +"-"+jurusanMengajar +"-"+fakultasMengajar +"-"+universitasMengajar +"-"+situasiDosen)
+}
+
+//call function here
+dataDosen (nip+"-"+namaDosen +"-"+tinggiBadanDosen +"-"+beratBadanDosen +"-"+usiaDosen +"-"+golonganDarahDosen +"-"+nomorTeleponDosen +"-"+agamaDosen+"-"+tanggalLahirDosen +"-"+asalDosen +"-"+sukuDosen +"-"+asalLulusanDosen +"-"+prestasiDosen +"-"+gelar +"-"+tanggalMulaiMengajar +"-"+golongan +"-"+jurusanMengajar +"-"+fakultasMengajar +"-"+universitasMengajar +"-"+situasiDosen)
+
+console.log ("Data Pegawai Teknik Elektro")
+//inisiasi Variable
+
+    let nipPegawai  = "122005541107"
+    let namaPegawai ="Ari"
+    let tinggiBadanPegawai ="170"
+    let beratBadanPegawai="90"
+    let usiaPegawai="50"
+    let golonganDarahPegawai=" O "
+    let nomorTeleponPegawai=" 082123456789 "
+    let agamaPegawai=" Islam "
+    let tanggalLahirPegawai=" 12 September 2001 "
+    let asalPegawai =" Lombok "
+    let gajipPerpusPegawai =" 5 juta "
+    let asalLulusanPegawai=" Universitas Mataram "
+    let prestasiPegawai="Juara 1 Lomba Melukis"
+    let gelarPegawai ="Sarjana Teknik Elektro"
+    let tanggalMulaiPegawai="1 Agustus 2020"
+    let golonganPegawai ="IIA"
+    const bidang="Teknik Elektro"
+    const fakultasBidang="Teknik"
+    const universitasBidang="Udayana"
+    let situasiPegawai="Aktif"
+//declare function here
+function dataPegawai(nipPegawai,namaPegawai,tinggiBadanPegawai,beratBadanPegawai,usiaPegawai,golonganDarahPegawai,nomorTeleponPegawai,agamaPegawai,tanggalLahirPegawai,asalPegawai,gajipPerpusPegawai,asalLulusanPegawai,prestasiPegawai,gelarPegawai,tanggalMulaiPegawai,golonganPegawai,bidang,fakultasBidang,universitasBidang,situasiPegawai){
+    
+    console.log(nipPegawai+"-"+namaPegawai +"-"+tinggiBadanPegawai +"-"+beratBadanPegawai +"-"+usiaPegawai +"-"+golonganDarahPegawai +"-"+nomorTeleponPegawai +"-"+agamaPegawai+"-"+tanggalLahirPegawai +"-"+asalPegawai +"-"+gajipPerpusPegawai +"-"+asalLulusanPegawai +"-"+prestasiPegawai +"-"+gelarPegawai +"-"+tanggalMulaiPegawai +"-"+golonganPegawai +"-"+bidang +"-"+fakultasBidang +"-"+universitasBidang +"-"+situasiPegawai)
+}
+
+//call function here
+dataPegawai (nipPegawai+"-"+namaPegawai +"-"+tinggiBadanPegawai +"-"+beratBadanPegawai +"-"+usiaPegawai +"-"+golonganDarahPegawai +"-"+nomorTeleponPegawai +"-"+agamaPegawai+"-"+tanggalLahirPegawai +"-"+asalPegawai +"-"+gajipPerpusPegawai +"-"+asalLulusanPegawai +"-"+prestasiPegawai +"-"+gelarPegawai +"-"+tanggalMulaiPegawai +"-"+golonganPegawai +"-"+bidang +"-"+fakultasBidang +"-"+universitasBidang +"-"+situasiPegawai)
+
+console.log ("Data Buku di Perpustakaan Teknik Elektro")
+//inisiasi Variable
+
+    let nomorBuku  = "0001"
+    let judulBuku ="Kalkulus 2"
+    let namaPengarang ="pradnya"
+    let tahunTerbit="1990"
+    let penerbit="PT.Gramedia"
+    let tempatTerbit="Kuta"
+    let jumlahCetakan="3"
+    let ISBN="978-979-107-882-5"
+    let jenisBuku="Basic Sains"
+    let jumlahBukuTersedia=" 3 "
+    let jumlahBukuDipinjam="1"
+    let beratBuku ="100 gr "
+    let panjangBuku=" 10 cm "
+    let jumlahHalaman="30 halaman "
+    let sumber="Dinas Perpustakaan dan Kearsipan"
+    let tanggalMulaiTersedia="1 Agustus 2020"
+    let cakupan="Mahasiswa Teknik Elektro"
+    const durasiPeminjaman="1 bulan"
+    const dendaKehilangan="500ribu"
+    let situasiBuku="Tersedia"
+//declare function here
+function dataBuku(nomorBuku,judulBuku,namaPengarang,tahunTerbit,penerbit,tempatTerbit,jumlahCetakan,ISBN,jenisBuku,jumlahBukuTersedia,jumlahBukuDipinjam,beratBuku,panjangBuku,jumlahHalaman,sumber,tanggalMulaiTersedia,cakupan,durasiPeminjaman,dendaKehilangan,situasiBuku){
+    
+    console.log(nomorBuku+"-"+judulBuku +"-"+namaPengarang +"-"+tahunTerbit +"-"+penerbit +"-"+tempatTerbit +"-"+jumlahCetakan +"-"+ISBN+"-"+jenisBuku +"-"+jumlahBukuTersedia +"-"+jumlahBukuDipinjam +"-"+beratBuku +"-"+panjangBuku +"-"+jumlahHalaman +"-"+sumber +"-"+tanggalMulaiTersedia +"-"+cakupan +"-"+durasiPeminjaman +"-"+dendaKehilangan +"-"+situasiBuku)
+}
+
+//call function here
+dataBuku (nomorBuku+"-"+judulBuku +"-"+namaPengarang +"-"+tahunTerbit +"-"+penerbit +"-"+tempatTerbit +"-"+jumlahCetakan +"-"+ISBN+"-"+jenisBuku +"-"+jumlahBukuTersedia +"-"+jumlahBukuDipinjam +"-"+beratBuku +"-"+panjangBuku +"-"+jumlahHalaman +"-"+sumber +"-"+tanggalMulaiTersedia +"-"+cakupan +"-"+durasiPeminjaman +"-"+dendaKehilangan +"-"+situasiBuku)
+
+console.log ("Data Anggota Perpus 2023")
+let idAnggota  = "AP0001"
+let namaAnggota ="MITHA"
+let tinggiBadanAnggota="160"
+let beratBadanAnggota="50"
+let usiaAnggota="20"
+let golonganDarahAnggota=" O "
+let nomorTeleponAnggota=" 0821234523132"
+let tanggalLahirAnggota=" 12 April 2001 "
+let asalAnggota=" Bali "
+let jenisAnggota =" Reguler "
+let asalSekolahAnggota=" Sman 1 Kuta "
+let prestasiAnggota="Juara 1 Lomba Debat"
+let jenisBukuDisukai="Novel"
+let jumlahBukuDibaca=" 12 "
+let tanggalMasukAnggota="1 Agustus 2020"
+let jumlahDenda="0 rupiah"
+let jurusanAnggota="Hukum"
+let fakultasAnggota="Hukum"
+const universitasAnggota="Udayana"
+let situasiKeanggotaan="Aktif"
+
+//declare function here
+function dataAnggota(idAnggota,namaAnggota,tinggiBadanAnggota,beratBadanAnggota,usiaAnggota,golonganDarahAnggota,nomorTeleponAnggota,tanggalLahirAnggota,asalAnggota,jenisAnggota,asalSekolahAnggota,prestasiAnggota,jenisBukuDisukai,jumlahBukuDibaca,tanggalMasukAnggota,jumlahDenda,jurusanAnggota,fakultasAnggota,universitasAnggota,situasiKeanggotaan){
+console.log(idAnggota+"-"+namaAnggota +"-"+tinggiBadanAnggota +"-"+beratBadanAnggota +"-"+usiaAnggota +"-"+golonganDarahAnggota +"-"+nomorTeleponAnggota+"-"+tanggalLahirAnggota +"-"+asalAnggota +"-"+jenisAnggota +"-"+asalSekolahAnggota+"-"+prestasiAnggota+"-"+jenisBukuDisukai+"-"+jumlahBukuDibaca +"-"+tanggalMasukAnggota +"-"+jumlahDenda +"-"+jurusanAnggota +"-"+fakultasAnggota +"-"+universitasAnggota +"-"+situasiKeanggotaan)
+}
+
+//call function here
+dataAnggota(idAnggota+"-"+namaAnggota +"-"+tinggiBadanAnggota +"-"+beratBadanAnggota +"-"+usiaAnggota +"-"+golonganDarahAnggota +"-"+nomorTeleponAnggota+"-"+tanggalLahirAnggota +"-"+asalAnggota +"-"+jenisAnggota +"-"+asalSekolahAnggota+"-"+prestasiAnggota+"-"+jenisBukuDisukai+"-"+jumlahBukuDibaca +"-"+tanggalMasukAnggota +"-"+jumlahDenda +"-"+jurusanAnggota +"-"+fakultasAnggota +"-"+universitasAnggota +"-"+situasiKeanggotaan)
+
+console.log ("Data Pegawai Perpus 2023")
+let idpPerpus  = "PP0001"
+let namapPerpus="Sakarani"
+let tinggiBadanpPerpus="160"
+let beratBadanpPerpus="50"
+let usiapPerpus="20"
+let golonganDarahpPerpus=" O "
+let nomorTeleponpPerpus=" 0821234523132"
+let tanggalLahirpPerpus=" 13 April 2001 "
+let asalpPerpus=" Bali "
+let jenispPerpus =" Reguler "
+let asalSekolahpPerpus=" Sman 1 Kuta Selatan "
+let prestasipPerpus="Juara 1 Lomba Makan Kerupuk"
+let letakTugas="Novel"
+let jumlahJamKerjapPerpus=" 12 "
+let tanggalMasukpPerpus="1 Agustus 2020"
+let gajipPerpus="2 juta rupiah"
+let jurusanpPerpus="Hukum"
+let fakultaspPerpus="Hukum"
+const universitaspPerpus="Udayana"
+let situasiKepPerpusan="Aktif"
+
+//declare function here
+function datapPerpus(idpPerpus,namapPerpus,tinggiBadanpPerpus,beratBadanpPerpus,usiapPerpus,golonganDarahpPerpus,nomorTeleponpPerpus,tanggalLahirpPerpus,asalpPerpus,jenispPerpus,asalSekolahpPerpus,prestasipPerpus,letakTugas,jumlahJamKerjapPerpus,tanggalMasukpPerpus,gajipPerpus,jurusanpPerpus,fakultaspPerpus,universitaspPerpus,situasiKepPerpusan){
+console.log(idpPerpus+"-"+namapPerpus +"-"+tinggiBadanpPerpus +"-"+beratBadanpPerpus +"-"+usiapPerpus +"-"+golonganDarahpPerpus +"-"+nomorTeleponpPerpus+"-"+tanggalLahirpPerpus +"-"+asalpPerpus +"-"+jenispPerpus +"-"+asalSekolahpPerpus+"-"+prestasipPerpus+"-"+letakTugas+"-"+jumlahJamKerjapPerpus +"-"+tanggalMasukpPerpus +"-"+gajipPerpus +"-"+jurusanpPerpus +"-"+fakultaspPerpus +"-"+universitaspPerpus +"-"+situasiKepPerpusan)
+}
+
+//call function here
+datapPerpus(idpPerpus+"-"+namapPerpus +"-"+tinggiBadanpPerpus +"-"+beratBadanpPerpus +"-"+usiapPerpus +"-"+golonganDarahpPerpus +"-"+nomorTeleponpPerpus+"-"+tanggalLahirpPerpus +"-"+asalpPerpus +"-"+jenispPerpus +"-"+asalSekolahpPerpus+"-"+prestasipPerpus+"-"+letakTugas+"-"+jumlahJamKerjapPerpus +"-"+tanggalMasukpPerpus +"-"+gajipPerpus +"-"+jurusanpPerpus +"-"+fakultaspPerpus +"-"+universitaspPerpus +"-"+situasiKepPerpusan)
+
+console.log("Data Pengurus Kelompok Studi 2023")
+let nimPKS  = "122005541107"
+let namaPKS ="Ni Nyoman"
+let tinggiBadanPKS ="155"
+let beratBadanPKS="50"
+let usiaPKS="21"
+let golonganDarahPKS=" O "
+let nomorTeleponPKS=" 082123456134 "
+let agamaPKS=" Islam "
+let tanggalLahirPKS=" 12 Maret 2002 "
+let asalPKS =" Bali "
+let sukuPKS=" Bali "
+let lamaJabatanPKS =" 1 tahun "
+let prestasiPKS="Juara 1 Lomba Line Follower"
+let jabatanPKS ="ketua"
+let tanggalMasukPKS="1 Agustus 2023"
+let jalurMasukPKS="SBMPTN"
+const jurusanPKS="Hukum"
+const fakultasPKS="Hukum"
+const universitasPKS="Universitas Pendidikan Nasional"
+let situasiPKS="Aktif"
+
+function dataPKS(nimPKS,namaPKS,tinggiBadanPKS,beratBadanPKS,usiaPKS,golonganDarahPKS,nomorTeleponPKS,agamaPKS,tanggalLahirPKS,asalPKS,sukuPKS,lamaJabatanPKS,prestasiPKS,jabatanPKS,tanggalMasukPKS,jalurMasuk,PKS,fakultasPKS,universitasPKS,situasiPKS){
+    
+    console.log(nimPKS+"-"+namaPKS +"-"+tinggiBadanPKS +"-"+beratBadanPKS +"-"+usiaPKS +"-"+golonganDarahPKS +"-"+nomorTeleponPKS +"-"+agamaPKS+"-"+tanggalLahirPKS +"-"+asalPKS +"-"+sukuPKS +"-"+lamaJabatanPKS +"-"+prestasiPKS +"-"+jabatanPKS +"-"+tanggalMasukPKS +"-"+jalurMasukPKS +"-"+jurusanPKS +"-"+fakultasPKS +"-"+universitasPKS +"-"+situasiPKS)
+}
+
+//call function here
+dataPKS (nimPKS+"-"+namaPKS +"-"+tinggiBadanPKS +"-"+beratBadanPKS +"-"+usiaPKS +"-"+golonganDarahPKS +"-"+nomorTeleponPKS +"-"+agamaPKS+"-"+tanggalLahirPKS +"-"+asalPKS +"-"+suku +"-"+lamaJabatanPKS +"-"+prestasiPKS +"-"+jabatanPKS +"-"+tanggalMasukPKS +"-"+jalurMasukPKS +"-"+jurusanPKS +"-"+fakultasPKS +"-"+universitasPKS +"-"+situasiPKS)
+
+
+console.log("Data Mahasiswa MBKM  Dikti 2023")
+let nimmhsMBKMD  = "2005541155"
+let namamhsMBKMD ="DIANA"
+let tinggiBadanpmhsMBKMD ="165"
+let beratBadanpmhsMBKMD="70"
+let usiapmhsMBKMD="21"
+let golonganDarahpmhsMBKMD=" A "
+let nomorTeleponpmhsMBKMD=" 082123456134 "
+let agamapmhsMBKMD=" Islam "
+let tanggalLahirpmhsMBKMD=" 12 Maret 2002 "
+let asalpmhsMBKMD =" Bali "
+let sukupmhsMBKMD=" Bali "
+let lamaMasaStudiMBKMD =" 1 tahun "
+let prestasipmhsMBKMD="Juara 1 Lomba Badminton"
+let jenisKelaminmhsMBKMD ="Perempuan"
+let tanggalMasukMBKMD="1 Agustus 2022"
+let jenisMBKMD="PKM"
+const jurusanpmhsMBKMD="Teknik Mesin"
+const fakultaspmhsMBKMD="Teknik"
+const universitaspmhsMBKMD="Universitas Pendidikan Nasional"
+let situasipmhsMBKMD="Aktif"
+
+function datapmhsMBKMD(nimmhsMBKMD,namamhsMBKMD,tinggiBadanpmhsMBKMD,beratBadanpmhsMBKMD,usiapmhsMBKMD,golonganDarahpmhsMBKMD,nomorTeleponpmhsMBKMD,agamapmhsMBKMD,tanggalLahirpmhsMBKMD,asalpmhsMBKMD,sukupmhsMBKMD,lamaMasaStudiMBKMD,prestasipmhsMBKMD,jenisKelaminmhsMBKMD,tanggalMasukMBKMD,jenisMBKMD,jurusanpmhsMBKMD,fakultaspmhsMBKMD,universitaspmhsMBKMD,situasipmhsMBKMD){
+    
+    console.log(nimmhsMBKMD+"-"+namamhsMBKMD +"-"+tinggiBadanpmhsMBKMD +"-"+beratBadanpmhsMBKMD +"-"+usiapmhsMBKMD +"-"+golonganDarahpmhsMBKMD +"-"+nomorTeleponpmhsMBKMD +"-"+agamapmhsMBKMD+"-"+tanggalLahirpmhsMBKMD +"-"+asalpmhsMBKMD +"-"+sukupmhsMBKMD +"-"+lamaMasaStudiMBKMD +"-"+prestasipmhsMBKMD +"-"+jenisKelaminmhsMBKMD +"-"+tanggalMasukMBKMD +"-"+jenisMBKMD+"-"+jurusanpmhsMBKMD +"-"+fakultaspmhsMBKMD +"-"+universitaspmhsMBKMD +"-"+situasipmhsMBKMD)
+}
+
+//call function here
+datapmhsMBKMD (nimmhsMBKMD+"-"+namamhsMBKMD +"-"+tinggiBadanpmhsMBKMD +"-"+beratBadanpmhsMBKMD +"-"+usiapmhsMBKMD +"-"+golonganDarahpmhsMBKMD +"-"+nomorTeleponpmhsMBKMD +"-"+agamapmhsMBKMD+"-"+tanggalLahirpmhsMBKMD +"-"+asalpmhsMBKMD +"-"+sukupmhsMBKMD +"-"+lamaMasaStudiMBKMD +"-"+prestasipmhsMBKMD +"-"+jenisKelaminmhsMBKMD +"-"+tanggalMasukMBKMD +"-"+jenisMBKMD +"-"+jurusanpmhsMBKMD +"-"+fakultaspmhsMBKMD +"-"+universitaspmhsMBKMD +"-"+situasipmhsMBKMD)
+
+console.log("Data UKM Universitas Udayana")
+let idUKM  = "2005541155"
+let namaUKM ="DIANA"
+let jenisUKM ="165"
+let tahunBerdiriUKM="70"
+let lokasiUKM="21"
+let jumlahPengurusUKM=" A "
+let jumlahAnggotaUKM=" 082123456134 "
+let namaKetuaUKM=" Islam "
+let namaWakilUKM=" 12 Maret 2002 "
+let namaSekretarisUKM=" Bali "
+let jumlahPendanaanUKM=" Bali "
+let sumberPendanaanUKM=""
+let jumlahPrestasiUKMDikti=""
+let jumlahPrestasiUKMMandiri =" 1 tahun "
+let jumlahAngkatanUKM="Juara 1 Lomba Badminton"
+let golonganUKM="Perempuan"
+let jumlahProdiDalamUKM="Teknik Mesin"
+let jumlahfakultasDalamsUKM="Teknik"
+let jumlahDosenPembina="Universitas Pendidikan Nasional"
+let situasiUKM="Aktif"
+
+function dataUKM(idUKM,namaUKM,jenisUKM,tahunBerdiriUKM,lokasiUKM,jumlahPengurusUKM,jumlahAnggotaUKM,lokasiUKM,jumlahPengurusUKM,jumlahAnggotaUKM,namaKetuaUKM,namaWakilUKM,namaSekretarisUKM,jumlahPendanaanUKM,sumberPendanaanUKM,jumlahPrestasiUKMDikti,jumlahPrestasiUKMMandiri,jumlahAngkatanUKM,golonganUKM,situasipmhsMBKMD){
+    
+    console.log(idUKM+"-"+namaUKM +"-"+jenisUKM +"-"+tahunBerdiriUKM +"-"+lokasiUKM +"-"+jumlahPengurusUKM +"-"+jumlahAnggotaUKM +"-"+namaKetuaUKM+"-"+namaWakilUKM+"-"+namaSekretarisUKM +"-"+sukupmhsMBKMD +"-"+lamaMasaStudiMBKMD +"-"+prestasipmhsMBKMD +"-"+jenisKelaminmhsMBKMD +"-"+tanggalMasukMBKMD +"-"+jenisMBKMD+"-"+jurusanpmhsMBKMD +"-"+fakultaspmhsMBKMD +"-"+universitaspmhsMBKMD +"-"+situasipmhsMBKMD)
+}
+
+//call function here
+datapmhsMBKMD (nimmhsMBKMD+"-"+namamhsMBKMD +"-"+tinggiBadanpmhsMBKMD +"-"+beratBadanpmhsMBKMD +"-"+usiapmhsMBKMD +"-"+golonganDarahpmhsMBKMD +"-"+nomorTeleponpmhsMBKMD +"-"+agamapmhsMBKMD+"-"+tanggalLahirpmhsMBKMD +"-"+asalpmhsMBKMD +"-"+sukupmhsMBKMD +"-"+lamaMasaStudiMBKMD +"-"+prestasipmhsMBKMD +"-"+jenisKelaminmhsMBKMD +"-"+tanggalMasukMBKMD +"-"+jenisMBKMD +"-"+jurusanpmhsMBKMD +"-"+fakultaspmhsMBKMD +"-"+universitaspmhsMBKMD +"-"+situasipmhsMBKMD)
+
